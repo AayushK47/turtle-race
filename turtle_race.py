8036a50dc18dba05a02b245c06703109b1c9c80d
Aayush Kurup <aayushkurup786@outlook.com>
2018-08-15 02:29:10 +0530

    4th commit: modularized the code

--- turtle_race.py
+++ turtle_race.py
@@ -1,7 +1,7 @@
 '''
 Project Name: Turtle race
 Author: Aayush Kurup
-Libraries used: turtle, random.randint
+Libraries used: turtle, random.randint, time.sleep
 Start Date: 12-08-2018
 Completion date: 12-08-2018
 '''
@@ -38,8 +38,12 @@ def set_race(turtle_main):  # this function will draw the race start and end lin
     draw_track(turtle_main)
     turtle_main.ht()
 
-def set_players():      # this function will create all the players and set them
-                        # on their start position
+'''
+The below function will initialize all the players/turtles ,
+set their shape and value
+and return the instance of those players/turtles
+'''
+def set_players():
 
     player_1 = turtle.Turtle()   # defining player 1      
     player_1.shape('turtle')
@@ -71,12 +75,17 @@ def set_players():      # this function will create all the players and set them
 
     return (player_1,player_2,player_3,player_4)    # return instance of each player
 
-def start_race(turtle_main):        # this function will start the race
+'''
+the below function will start the race
+'''
+def start_race(turtle_main):
     
     player_1,player_2,player_3,player_4 = set_players()     # init Players
     flag = 1        # this flag will check if we have a winner or not
 
-    # the race (loop) will continue until all players reach the end line i.e x-cordinate=200
+    '''
+    the below loop will continue until all players reach the end line i.e x-cordinate=200
+    '''
     while(player_1.xcor()<200 or player_2.xcor()<200 or player_3.xcor()<200 or player_4.xcor()<200):
 
         if(player_1.xcor()<200):    # if the player has not reached the end line
@@ -94,47 +103,79 @@ def start_race(turtle_main):        # this function will start the race
         if(flag):   # if we have a winner
             
             if(player_1.xcor()>=200):  # if player 1 is the winner
+                '''
+                Then display player 1 wins and set flag = 0 (don't check for winner again)
+                '''
                 turtle_main.undo()
                 turtle_main.write('Player 1 wins',align='center',font=("Arial", 20, "normal"))
-                flag = 0        # Then display player 1 wins and set flag = 0 (don't check for winner again)
+                flag = 0
 
             elif(player_2.xcor()>=200): # if player 2 is the winner
+                '''
+                Then display player 2 wins and set flag = 0 (don't check for winner again)
+                '''
                 turtle_main.undo()
                 turtle_main.write('Player 2 wins',align='center',font=("Arial", 20, "normal"))
-                flag = 0        # Then display player 2 wins and set flag = 0 (don't check for winner again)
+                flag = 0
             
             elif(player_3.xcor()>=200):   # if player 3 is the winner
+                '''
+                Then display player 3 wins and set flag = 0 (don't check for winner again)
+                '''
                 turtle_main.undo()
                 turtle_main.write('Player 3 wins',align='center',font=("Arial", 20, "normal"))
-                flag = 0        # Then display player 3 wins and set flag = 0 (don't check for winner again)
+                flag = 0    
             
             elif(player_4.xcor()>=200):     # if player 4 is the winner
+                '''
+                Then display player 4 wins and set flag = 0 (don't check for winner again)
+                '''
                 turtle_main.undo()
                 turtle_main.write('Player 4 wins',align='center',font=("Arial", 20, "normal"))
-                flag = 0        # Then display player 4 wins and set flag = 0 (don't check for winner again)
+                flag = 0
 
 
-turtle_main = turtle.Turtle()       # the main turtle that willl draw the race track
-scr = turtle.Screen()           # Instance of the screen
-scr.title('Turtle Race')
 
 '''
-The 3 below lines will display text 'Turtle Race' in the position (0,150)
+A timer is implemented in the below function
+The race will start after 3 seconds
 '''
+def timer(turtle_main):
+    for i in reversed(range(3)):
+        turtle_main.write('The race will start in {}'.format(i+1),align='center',font=("Arial", 20, "normal"))
+        sleep(1)
+        turtle_main.undo()
+
+'''
+The function will display text 'Turtle Race' in the position (0,150)
+'''
+def display_turtle_race(turtle_main):
+    turtle_main.pu()        
+    turtle_main.setpos(0,150)
+    turtle_main.write('Turtle Race',align='center',font=("Arial", 20, "bold"))
 
 
-turtle_main.pu()        
-turtle_main.setpos(0,150)
-turtle_main.write('Turtle Race',align='center',font=("Arial", 20, "bold"))
+'''
+The main function to execute the program
+'''
+def main():
+    
+    turtle_main = turtle.Turtle()       # the main turtle that willl draw the race track
+    scr = turtle.Screen()           # Instance of the screen
+    scr.title('Turtle Race')
 
-set_race(turtle_main) # this function will draw the race track
-turtle_main.setpos(0,-250) # will set the main turtle at position (0,-250)
+    display_turtle_race(turtle_main)
+    
+    set_race(turtle_main) # this function will draw the race track
+    turtle_main.setpos(0,-250) # this statement will set the main turtle at position (0,-250)
 
-for i in reversed(range(3)):
-    turtle_main.write('The race will start in {}'.format(i+1),align='center',font=("Arial", 20, "normal"))
-    sleep(1)
-    turtle_main.undo()
+    timer(turtle_main)
+    turtle_main.write('Let\'s go',align='center',font=("Arial", 20, "normal"))
+    start_race(turtle_main)  # this function will start the race
+    turtle.exitonclick()    # when the race ends the clicking the screen will close the window
 
-turtle_main.write('Let\'s go',align='center',font=("Arial", 20, "normal"))
-start_race(turtle_main)  # this function will start the race
-turtle.exitonclick()    # when the race ends the clicking the screen will close the window
+'''
+The main function will not execute if the file is imported
+'''
+if __name__ == '__main__':
+    main()
